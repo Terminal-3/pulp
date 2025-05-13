cfabe78df1523b56a9f0d208cef7104a203424c6
Ashu <148854694+ashu-t3@users.noreply.github.com>
2025-05-13 23:01:45 +0530

    Revert "Merge branch 'main' into implserde" (#2)
    This reverts commit 15960b13fa0f53c9ac507fdc3c0aa4b17a71b6e8, reversing
    changes made to 69980e2dc564055aedb1a6263e011fc57dd4aac5.
    
    Co-authored-by: Ashutosh Nanda <ashu8989@gmail.com>

--- pulp/src/core_arch/mod.rs
+++ pulp/src/core_arch/mod.rs
@@ -1,43 +1,44 @@
 mod arch {
-	#[cfg(target_arch = "aarch64")]
-	pub use core::arch::aarch64::*;
-	#[cfg(target_arch = "wasm32")]
-	pub use core::arch::wasm32::*;
-	#[cfg(target_arch = "x86")]
-	pub use core::arch::x86::*;
-	#[cfg(target_arch = "x86_64")]
-	pub use core::arch::x86_64::*;
+    #[cfg(target_arch = "aarch64")]
+    pub use core::arch::aarch64::*;
+    #[cfg(target_arch = "wasm32")]
+    pub use core::arch::wasm32::*;
+    #[cfg(target_arch = "x86")]
+    pub use core::arch::x86::*;
+    #[cfg(target_arch = "x86_64")]
+    pub use core::arch::x86_64::*;
 
-	#[cfg(all(feature = "nightly", target_arch = "arm"))]
-	pub use core::arch::arm::*;
-	#[cfg(all(feature = "nightly", target_arch = "mips"))]
-	pub use core::arch::mips::*;
-	#[cfg(all(feature = "nightly", target_arch = "mips64"))]
-	pub use core::arch::mips64::*;
-	#[cfg(all(feature = "nightly", target_arch = "powerpc"))]
-	pub use core::arch::powerpc::*;
-	#[cfg(all(feature = "nightly", target_arch = "powerpc64"))]
-	pub use core::arch::powerpc64::*;
-	#[cfg(all(feature = "nightly", target_arch = "riscv32"))]
-	pub use core::arch::riscv32::*;
-	#[cfg(all(feature = "nightly", target_arch = "riscv64"))]
-	pub use core::arch::riscv64::*;
-	#[cfg(all(feature = "nightly", target_arch = "wasm64"))]
-	pub use core::arch::wasm64::*;
+    #[cfg(all(feature = "nightly", target_arch = "arm"))]
+    pub use core::arch::arm::*;
+    #[cfg(all(feature = "nightly", target_arch = "mips"))]
+    pub use core::arch::mips::*;
+    #[cfg(all(feature = "nightly", target_arch = "mips64"))]
+    pub use core::arch::mips64::*;
+    #[cfg(all(feature = "nightly", target_arch = "nvptx"))]
+    pub use core::arch::nvptx::*;
+    #[cfg(all(feature = "nightly", target_arch = "powerpc"))]
+    pub use core::arch::powerpc::*;
+    #[cfg(all(feature = "nightly", target_arch = "powerpc64"))]
+    pub use core::arch::powerpc64::*;
+    #[cfg(all(feature = "nightly", target_arch = "riscv32"))]
+    pub use core::arch::riscv32::*;
+    #[cfg(all(feature = "nightly", target_arch = "riscv64"))]
+    pub use core::arch::riscv64::*;
+    #[cfg(all(feature = "nightly", target_arch = "wasm64"))]
+    pub use core::arch::wasm64::*;
 }
 
 #[allow(unused_macros)]
 macro_rules! delegate {
-    ({$(
+    ($(
         $(#[$attr: meta])*
         $(unsafe $($placeholder: lifetime)?)?
         fn $func: ident $(<$(const $generic: ident: $generic_ty: ty),* $(,)?>)?(
             $($arg: ident: $ty: ty),* $(,)?
         ) $(-> $ret: ty)?;
-    )*}) => {
+    )*) => {
         $(
             $(#[$attr])*
-            #[allow(clippy::missing_safety_doc)]
             #[inline(always)]
             pub $(unsafe $($placeholder)?)? fn $func $(<$(const $generic: $generic_ty,)*>)?(self, $($arg: $ty,)*) $(-> $ret)? {
                 unsafe { arch::$func $(::<$($generic,)*>)?($($arg,)*) }
@@ -49,28 +50,28 @@ macro_rules! delegate {
 #[cfg(all(feature = "std", any(target_arch = "x86", target_arch = "x86_64")))]
 #[macro_export]
 macro_rules! feature_detected {
-	($feature: tt) => {
-		::std::is_x86_feature_detected!($feature)
-	};
+    ($feature: tt) => {
+        ::std::is_x86_feature_detected!($feature)
+    };
 }
 
 #[cfg(all(feature = "std", target_arch = "aarch64"))]
 #[macro_export]
 macro_rules! feature_detected {
-	($feature: tt) => {
-		::std::arch::is_aarch64_feature_detected!($feature)
-	};
+    ($feature: tt) => {
+        ::std::arch::is_aarch64_feature_detected!($feature)
+    };
 }
 
 #[cfg(any(
-	not(feature = "std"),
-	not(any(target_arch = "x86", target_arch = "x86_64", target_arch = "aarch64"))
+    not(feature = "std"),
+    not(any(target_arch = "x86", target_arch = "x86_64", target_arch = "aarch64"))
 ))]
 #[macro_export]
 macro_rules! feature_detected {
-	($feature: tt) => {
-		cfg!(target_feature = $feature)
-	};
+    ($feature: tt) => {
+        cfg!(target_arch = $feature)
+    };
 }
 
 #[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
@@ -103,7 +104,7 @@ macro_rules! __impl_type {
     ("avx512pf") => { $crate::core_arch::x86::Avx512pf };
     ("avx512bw") => { $crate::core_arch::x86::Avx512bw };
     ("avx512dq") => { $crate::core_arch::x86::Avx512dq };
-    ("avx512vl") => { $crate::core_arch::x86::Avx512f };
+    ("avx512vl") => { $crate::core_arch::x86::Avx512vl };
     ("avx512ifma") => { $crate::core_arch::x86::Avx512ifma };
     ("avx512vbmi") => { $crate::core_arch::x86::Avx512vbmi };
     ("avx512vpopcntdq") => { $crate::core_arch::x86::Avx512vpopcntdq };
@@ -182,14 +183,6 @@ macro_rules! __impl_type {
     ("asimd") => { $crate::core_arch::aarch64::Asimd };
 }
 
-#[cfg(not(any(target_arch = "x86_64", target_arch = "x86", target_arch = "aarch64",)))]
-#[doc(hidden)]
-#[rustfmt::skip]
-#[macro_export]
-macro_rules! __impl_type {
-    ($tt: tt) => { compile_error!("unsupported arch") }
-}
-
 #[macro_export]
 macro_rules! simd_type {
     (
@@ -200,28 +193,12 @@ macro_rules! simd_type {
             }
         )*
     ) => {
-        $crate::simd_type!({$(
-            $(#[$attr])*
-            $vis struct $name {
-                $($feature_vis $ident: target_feature!($feature),)*
-            }
-        )*});
-    };
-
-    ({
-        $(
-            $(#[$attr: meta])*
-            $vis: vis struct $name: ident {
-                $($feature_vis: vis $ident: ident: $_: ident!($feature: tt)),* $(,)?
-            }
-        )*
-    }) => {
         $(
             #[allow(dead_code)]
             $(#[$attr])*
             #[derive(Clone, Copy, Debug)]
             $vis struct $name{
-                $($feature_vis $ident : $crate::core_arch::__impl_type!($feature),)*
+                $($feature_vis $ident : $crate::__impl_type!($feature),)*
             }
 
             #[allow(dead_code)]
@@ -234,9 +211,9 @@ macro_rules! simd_type {
                 /// - the required CPU features must be available.
                 #[inline]
                 pub unsafe fn new_unchecked() -> Self {
-                    unsafe{Self{
-                        $($ident: <$crate::core_arch::__impl_type!($feature)>::new_unchecked(),)*
-                    }}
+                    Self{
+                        $($ident: <$crate::__impl_type!($feature)>::new_unchecked(),)*
+                    }
                 }
 
                 /// Returns a SIMD token type if the required CPU features for this type are
@@ -244,9 +221,9 @@ macro_rules! simd_type {
                 #[inline]
                 pub fn try_new() -> Option<Self> {
                     if Self::is_available() {
-                        Some(unsafe{Self{
-                            $($ident: <$crate::core_arch::__impl_type!($feature)>::new_unchecked(),)*
-                        }})
+                        Some(Self{
+                            $($ident: <$crate::__impl_type!($feature)>::new_unchecked(),)*
+                        })
                     } else {
                         None
                     }
@@ -272,7 +249,7 @@ macro_rules! simd_type {
 
                 #[inline(never)]
                 fn __detect_is_available() -> bool {
-                    let out = true $(&& <$crate::core_arch::__impl_type!($feature)>::is_available())*;
+                    let out = true $(&& <$crate::__impl_type!($feature)>::is_available())*;
                     Self::__static_available().store(out as u8, ::core::sync::atomic::Ordering::Relaxed);
                     out
                 }
@@ -287,53 +264,10 @@ macro_rules! simd_type {
                 pub fn vectorize<F: $crate::NullaryFnOnce>(self, f: F) -> F::Output {
                     $(#[target_feature(enable = $feature)])*
                     #[inline]
-                	#[allow(clippy::too_many_arguments)]
-                    unsafe fn imp_fastcall<F: $crate::NullaryFnOnce>(
-                        f0: ::core::mem::MaybeUninit<::core::primitive::usize>,
-                        f1: ::core::mem::MaybeUninit<::core::primitive::usize>,
-                        f2: ::core::mem::MaybeUninit<::core::primitive::usize>,
-                        f3: ::core::mem::MaybeUninit<::core::primitive::usize>,
-                        f4: ::core::mem::MaybeUninit<::core::primitive::usize>,
-                        f5: ::core::mem::MaybeUninit<::core::primitive::usize>,
-                        f6: ::core::mem::MaybeUninit<::core::primitive::usize>,
-                        f7: ::core::mem::MaybeUninit<::core::primitive::usize>,
-                    ) -> F::Output {
-                        let f: F = core::mem::transmute_copy(&[f0, f1, f2, f3, f4, f5, f6, f7]);
+                    unsafe fn __impl<F: $crate::NullaryFnOnce>(f: F) -> F::Output {
                         f.call()
                     }
-
-                    $(#[target_feature(enable = $feature)])*
-                    #[inline]
-                    unsafe fn imp<F: $crate::NullaryFnOnce>(f: F) -> F::Output {
-                        f.call()
-                    }
-
-                    if const { ::core::mem::size_of::<F>() <= 8 * ::core::mem::size_of::<::core::primitive::usize>() } {
-                        union Pad<T> {
-                            t: ::core::mem::ManuallyDrop<T>,
-                            __u: ::core::mem::MaybeUninit<[usize; 8]>,
-                        }
-
-                        let f = Pad {
-                            t: ::core::mem::ManuallyDrop::new(f),
-                        };
-                        let p = (&f) as *const _ as *const ::core::mem::MaybeUninit<usize>;
-
-                        unsafe {
-                            imp_fastcall::<F>(
-                                *p.add(0),
-                                *p.add(1),
-                                *p.add(2),
-                                *p.add(3),
-                                *p.add(4),
-                                *p.add(5),
-                                *p.add(6),
-                                *p.add(7),
-                            )
-                        }
-                    } else {
-                        unsafe { imp(f) }
-                    }
+                    unsafe { __impl(f) }
                 }
 
                 /// Takes a proof of the existence of this SIMD token (`self`), and returns a
@@ -350,9 +284,108 @@ macro_rules! simd_type {
     };
 }
 
-#[rustfmt::skip]
-#[doc(hidden)]
-pub use __impl_type as __impl_type;
+macro_rules! internal_simd_type {
+    (
+        $(
+            $(#[$attr: meta])*
+            $vis: vis struct $name: ident {
+                $($feature_vis: vis $ident: ident: $feature: tt),* $(,)?
+            }
+        )*
+    ) => {
+        $(
+            #[repr(transparent)]
+            #[allow(dead_code)]
+            $(#[$attr])*
+            #[derive(Clone, Copy, Debug)]
+            $vis struct $name{
+                $($feature_vis $ident : __impl_type!($feature),)*
+            }
+
+            #[allow(dead_code)]
+            $(#[$attr])*
+            impl $name {
+                /// Returns a SIMD token type without checking if the required CPU features for
+                /// this type are available.
+                ///
+                /// # Safety
+                /// - the required CPU features must be available.
+                #[inline]
+                pub unsafe fn new_unchecked() -> Self {
+                    Self{
+                        $($ident: <__impl_type!($feature)>::new_unchecked(),)*
+                    }
+                }
+
+                /// Returns a SIMD token type if the required CPU features for this type are
+                /// available, otherwise returns `None`.
+                #[inline]
+                pub fn try_new() -> Option<Self> {
+                    if Self::is_available() {
+                        Some(Self{
+                            $($ident: <__impl_type!($feature)>::new_unchecked(),)*
+                        })
+                    } else {
+                        None
+                    }
+                }
+
+                #[inline(always)]
+                fn __static_available() -> &'static ::core::sync::atomic::AtomicU8 {
+                    static AVAILABLE: ::core::sync::atomic::AtomicU8 = ::core::sync::atomic::AtomicU8::new(u8::MAX);
+                    &AVAILABLE
+                }
+
+                /// Returns `true` if the required CPU features for this type are available,
+                /// otherwise returns `false`.
+                #[inline]
+                pub fn is_available() -> bool {
+                    let mut available = Self::__static_available().load(::core::sync::atomic::Ordering::Relaxed);
+                    if available == u8::MAX {
+                        available = Self::__detect_is_available() as u8;
+                    }
+
+                    available != 0
+                }
+
+                #[inline(never)]
+                fn __detect_is_available() -> bool {
+                    let out = true $(&& <__impl_type!($feature)>::is_available())*;
+                    Self::__static_available().store(out as u8, ::core::sync::atomic::Ordering::Relaxed);
+                    out
+                }
+
+                /// Vectorizes the given function as if the CPU features for this type were applied
+                /// to it.
+                ///
+                /// # Note
+                /// For the vectorization to work properly, the given function must be inlined.
+                /// Consider marking it as `#[inline(always)]`
+                #[inline(always)]
+                pub fn vectorize<F: $crate::NullaryFnOnce>(self, f: F) -> F::Output {
+                    $(#[target_feature(enable = $feature)])*
+                    #[inline]
+                    unsafe fn __impl<F: $crate::NullaryFnOnce>(f: F) -> F::Output {
+                        f.call()
+                    }
+                    unsafe { __impl(f) }
+                }
+
+                /// Takes a proof of the existence of this SIMD token (`self`), and returns a
+                /// persistent reference to it.
+                #[inline]
+                pub fn to_ref(self) -> &'static Self {
+                    const __ASSERT_ZST: () = {
+                        assert!(::core::mem::size_of::<$name>() == 0);
+                    };
+                    unsafe { &*::core::ptr::NonNull::dangling().as_ptr() }
+                }
+            }
+        )*
+    };
+}
+
+pub(crate) use internal_simd_type;
 
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 #[cfg_attr(docsrs, doc(cfg(any(target_arch = "x86", target_arch = "x86_64"))))]
